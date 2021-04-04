224497d447ecb0482c8e5c06691668a05e968cfb
dayotNicolas <nicolas.dayot@ynov.com>
2021-04-04 14:08:02 +0200

    css fenetre modales Fait, Calendrier ics Fait

--- BridgeMonitor/Controllers/HomeController.cs
+++ BridgeMonitor/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
-using System.Threading.Tasks;
 using BridgeMonitor.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Ical.Net;
+using Ical.Net.DataTypes;
+using Ical.Net.Serialization.iCalendar.Serializers;
+using System.Text;
 
 namespace BridgeMonitor.Controllers
 {
@@ -34,10 +37,49 @@ namespace BridgeMonitor.Controllers
             return View(boats);
         }
 
+        public IActionResult Calendar()
+        {
+            var arrayBoats = GetBoatsInfosFromApi();
+            return View(arrayBoats);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult DownloadCalendar()
+        {
+            List<Boat> boats = GetBoatsInfosFromApi();
+            for (int i = 0; i < boats.Count; i++)
+            {
+                if (boats[i].ClosingDate.CompareTo(DateTime.Now) < 0)
+                {
+                    boats.RemoveAt(i);
+                }
+            }
+       
+            boats.Sort((s1, s2) => DateTimeOffset.Compare(s1.ClosingDate, s2.ClosingDate));
+
+            var calendar = new Calendar();
+            foreach (Boat boat in boats)
+            {
+                var icalEvent = new Event
+                {
+                    Name = boat.BoatName,
+                    Description = string.Format("Type {0}", boat.ClosingType),
+                    Start = new CalDateTime(boat.ClosingDate),
+                    End = new CalDateTime(boat.ReopeningDate)
+                };
+
+                calendar.Events.Add(icalEvent);
+            }
+
+            var iCalSerializer = new CalendarSerializer();
+            string result = iCalSerializer.SerializeToString(calendar);
+
+            return File(Encoding.ASCII.GetBytes(result), "calendar/text", "calendar.ics");
         }
 
         private static List<Boat> GetBoatsInfosFromApi()
